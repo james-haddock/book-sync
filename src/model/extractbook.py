dfc33dec8e229ed2483f9fea8e4cf2fa1209ed39
James Haddock <j.haddock@outlook.com>
2023-08-09 14:50:31 +0100

    created book extract function

--- src/model/extractbook.py
+++ src/model/extractbook.py
@@ -1,19 +1,7 @@
-import tkinter as tk
-from tkinter import filedialog
-import sys
 import zipfile
-# sys.path.append("..")
 
-# root = tk.Tk()
-# root.withdraw()
-
-# book_path = filedialog.askopenfilename()
-
-book_path = 'data/epub/upp.epub'
-
-extract_directory = 'data/extracted'
-
-with zipfile.ZipFile(book_path, 'r') as zip:
-    zip.extractall(extract_directory)
-
-print("File successfully unzipped!")
+def extract_book(epub_book):
+    extract_directory = 'data/extracted'
+    with zipfile.ZipFile(epub_book, 'r') as zip:
+        zip.extractall(extract_directory)
+        print("File successfully unzipped!")
