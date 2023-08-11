db6febd7063a1f0b0ef9b3a48972f3a6700d8f55
James Haddock <123553781+james-haddock@users.noreply.github.com>
2023-08-11 12:12:23 +0100

    created get_spine() function

--- src/model/get_spine.py
+++ src/model/get_spine.py
@@ -1,17 +1,27 @@
 import xml.etree.ElementTree as ET
 
-
-
-
-def retrieve_opf_location(title):
+def get_opf_location(title):
     xml_path = f'data/extracted/{title}/META-INF/container.xml'
+    namespace = '{urn:oasis:names:tc:opendocument:xmlns:container}'
+    root = parse_and_get_root_xml(xml_path)
+    opf_location = root.find(f'{namespace}rootfiles/{namespace}rootfile').attrib['full-path']
+    return opf_location
+    
+def get_spine(title):
+    spine = []
+    opf_location = get_opf_location(title)
+    namespace = "{http://www.idpf.org/2007/opf}"
+    xml_path = f'data/extracted/{title}/{opf_location}'
+    root = parse_and_get_root_xml(xml_path)
+    for item in root.findall(f'{namespace}spine/{namespace}itemref'):
+        spine.append(item.attrib['idref'])
+    return spine
+
+def parse_and_get_root_xml(xml_path):
     tree = ET.parse(xml_path)
     root = tree.getroot()
-    opf_location = root.find('{urn:oasis:names:tc:opendocument:xmlns:container}rootfiles/{urn:oasis:names:tc:opendocument:xmlns:container}rootfile').attrib['full-path']
-    print(opf_location) 
- 
-    
-# def get_spine(title):
-#     opf_location = get_opf_location(title)
-#     spine = []
-#     with open(f'data/extracted/{title}/{opf_location}', 'r', encoding='utf-8') as opf_file:
+    return root
+
+book = 'Refactoring Improving the Design of Existing Code (Martin Fowler)'
+
+print(get_spine(book))
