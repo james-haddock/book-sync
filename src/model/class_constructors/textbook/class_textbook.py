603948d5b362cd5ec6731f5199cbf6bb42746ec1
James Haddock <123553781+james-haddock@users.noreply.github.com>
2023-10-09 12:05:42 +0100

    added type hints

--- src/model/class_constructors/textbook/class_textbook.py
+++ src/model/class_constructors/textbook/class_textbook.py
@@ -3,11 +3,11 @@ import os
 class Textbook:
     def __init__(self, UUID):
         from .textbook_initialiser import TextbookInitialiser
-        initialiser = TextbookInitialiser(UUID)
-        self.UUID = UUID
-        self.title = initialiser.title
-        self.cover = initialiser.cover
-        self.isbn = ''
-        self.book_position = 0
-        self.book_content = initialiser.html_manager.get_consolidated_html_path
-        self.book_base = os.path.dirname(self.book_content)
+        initialiser: TextbookInitialiser = TextbookInitialiser(UUID)
+        self.UUID: str = UUID
+        self.title: str = initialiser.title
+        self.cover: str = initialiser.cover
+        self.isbn: int = ''
+        self.book_position: int = 0
+        self.book_content: str = initialiser.html_manager.get_consolidated_html_path
+        self.book_base: str = os.path.dirname(self.book_content)
