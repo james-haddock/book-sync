edd61bb7cab65a635ce4761ba0bf3dae3feb8087
James Haddock <123553781+james-haddock@users.noreply.github.com>
2023-11-21 09:38:21 +0000

    linted controller.py

--- src/controller.py
+++ src/controller.py
@@ -12,9 +12,6 @@ from sqlalchemy.orm import Session, sessionmaker
 from model.db.crud import crud_textbook, crud_book
 from model.db.db_schema import db_schema
 from model.class_constructors import class_book
-from model.class_constructors.textbook import (class_textbook, xml_parser, 
-                                               book_consolidator, html_consolidation_manager, opf_extractor,
-                                               book_metadata_extractor, extract_book, epub_validator)
 from bs4 import BeautifulSoup, Doctype
 from model.db.crud import crud_book
 from model.db.db_manager import DatabaseManager
@@ -23,8 +20,10 @@ import copy
 from model.change_urls_to_presigned import change_urls_to_presigned
 from model.save_book_session import save_book_session_js
 from model.iframe_styles import get_iframe_styles
+from model.class_constructors.textbook import (class_textbook, xml_parser, 
+                                               book_consolidator, html_consolidation_manager, opf_extractor,
+                                               book_metadata_extractor, extract_book, epub_validator)
 
-change_urls_to_presigned = change_urls_to_presigned()
 
 s3 = boto3.client(
     's3',
@@ -36,33 +35,21 @@ s3 = boto3.client(
 
 aws_bucket=config('DO_BUCKET_NAME')
 
-
-
-
 class Config:
     DEBUG = config("DEBUG")
     SECRET_KEY = config("FLASK_SECRET_KEY")
     DATABASE_URL = config("DATABASE_URL")
     API_KEY = config("API_KEY")
 
-def has_file_extension(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].isalpha()
-
-current_directory = os.path.dirname(os.path.abspath(__file__))
-
 app = Flask(__name__, template_folder='', static_folder='')
-
-
 app.config.from_object(Config)
-
 crud_book = crud_book.CrudBook()
 crud_textbook = crud_textbook.CrudTextbook()
-
+change_urls_to_presigned = change_urls_to_presigned()
 
 @app.route("/")
 def index():
     return render_template('/templates/index.html')
-
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -71,13 +58,10 @@ def login():
     elif request.method == 'GET':
         pass
 
-
 @app.route("/register", methods=['GET', 'POST'])
 def registration_form():
     if request.method == 'GET':
         return render_template('/templates/register.html')
-
-    
 
 def get_s3_object_content(aws_bucket, object_name, s3):
     try:
@@ -86,7 +70,6 @@ def get_s3_object_content(aws_bucket, object_name, s3):
     except Exception as e:
         print(f"Error fetching S3 object {object_name}: {e}")
         return None
-
 
 @app.route('/book/<UUID>')
 def book(UUID):
@@ -100,7 +83,6 @@ def book(UUID):
         save_book_session = save_book_session_js(UUID)
         return render_template('templates/reader_nav.html', book_title=book_data['DBBook'].title, UUID=UUID, save_book_session=save_book_session)
 
-
 @app.route('/content/<UUID>')
 def content(UUID):
     with DatabaseManager() as session:
@@ -109,11 +91,10 @@ def content(UUID):
 
         html_content = get_s3_object_content(aws_bucket, textbook.book_content, s3)
         if not html_content:
-            return "Error fetching book content", 500
+            return "Error fetching book content", 500 
 
         amended_html = change_urls_to_presigned.change_html_links(html_content, UUID, aws_bucket, s3)
         return render_template_string(amended_html)
-    
     
 @app.route("/library", methods=['GET'])
 def library():
@@ -123,9 +104,6 @@ def library():
         for book in all_books_copy:
             book['DBTextbook'].cover = change_urls_to_presigned.generate_presigned_url(aws_bucket, book['DBTextbook'].cover, s3)
         return render_template('/templates/library.html', books=all_books_copy)
-
-
-
 
 @app.route("/upload", methods=['GET', 'POST'])
 def upload():
@@ -152,7 +130,6 @@ def upload():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('/templates/404.html'), 404
-
 
 if __name__ == '__main__':
     app.run(port=8000)         
